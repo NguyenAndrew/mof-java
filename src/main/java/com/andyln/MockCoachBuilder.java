3a578de0199c4a6e14b101c24c082941afd1d116
NguyenAndrew <anguyen29@wisc.edu>
2020-02-28 01:57:58 -0600

    Add javadocs to MockCoachBuilder

--- src/main/java/com/andyln/MockCoachBuilder.java
+++ src/main/java/com/andyln/MockCoachBuilder.java
@@ -5,21 +5,61 @@ public class MockCoachBuilder {
     MockCoachRunnable[] whens;
     MockCoachRunnable[] verifies;
 
+    /**
+     * Creates a builder used to construct a MockCoach. May provide a better experience than constructing MockCoach using its constructor.
+     * A single instance of MockCoach should be created for several unit tests, corresponding to a method of the class under test.
+     */
+    public MockCoachBuilder() {
+    }
+
+    /**
+     * Add mocks to MockCoachBuiler. Mocks are any object that are injected or autowired into an object under test.
+     * @param mocks
+     * @return MockCoachBuilder (with mocks)
+     */
     public MockCoachBuilder mock(Object... mocks) {
         this.mocks = mocks;
         return this;
     }
 
+    /**
+     * Add whens to MockCoachBuilder. Whens is an array of runnables, where each runnable may contain multiple when statements.
+     * <p>
+     * Example of a single when: {@code
+     * () -> &#123;
+     *     when(mock.method()).thenReturn(someValue);
+     *     when(mock.anotherMethod()).thenReturn(anotherValue);
+     * &#125;
+     * } </p>
+     * @param whens Multiple when runnables
+     * @return MockCoachBuilder (with whens)
+     */
     public MockCoachBuilder when(MockCoachRunnable... whens) {
         this.whens = whens;
         return this;
     }
 
+    /**
+     * Add verifies to MockCoachBuilder. Verifies is an array of runnables, where each runnable may contain multiple verify statements.
+     * <p>
+     * Example of a single verify: {@code
+     * () -> &#123;
+     *     verify(mock, times(1)).method();
+     *     verify(mock, times(1)).anotherMethod());
+     * &#125;
+     * } </p>
+     * @param verifies Multiple verify runnables
+     * @return MockCoachBuilder (with verifies)
+     */
     public MockCoachBuilder verify(MockCoachRunnable... verifies) {
         this.verifies = verifies;
         return this;
     }
 
+    /**
+     * Returns a new MockCoach using its mocks, whens, and verifies.
+     * @return MockCoach
+     */
     public MockCoach build() {
         return new MockCoach(mocks, whens, verifies);
     }
